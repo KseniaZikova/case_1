27338b12b59fd88702cfb9b1bbf2bc7f8379d71c
mashabat2 <mashabat2@mail.ru>
2019-02-12 15:47:02 +0700

    Masha's commit

--- base.py
+++ base.py
@@ -13,8 +13,32 @@ def check1():
     return number
 
 
+def check2():
+    while True:
+        try:
+            number = int(input("На какое количество мороженого вы закупитесь ресурсами?"))
+        except ValueError:
+            print('Неверный ввод данных.')
+            continue
+        else:
+            break
+    return number
+
+
+def check3():
+    while True:
+        try:
+            number = int(input("Отлично! Сколько мороженого вы готовы продать?"))
+        except ValueError:
+            print('Неверный ввод данных.')
+            continue
+        else:
+            break
+    return number
+
+
 def main():
-    d = {'client': 50, 'item': 100, 'money': 1000, 'crisis': 0, 'price': 25, 'day': 1}
+    d = {'client': 50, 'item': 100, 'money': 1000, 'crisis': 0, 'price': 25, 'day': 0}
 
     def PrintData(d):
         print('\n' * 80)
@@ -34,8 +58,48 @@ def main():
             '_____________________________________________________________________________________________________________')
 
     while True:
+        d['day'] += 1
         PrintData(d)
-        answer_donation = input('Не хотите ли пожертвовать некоторую сумму в благ.фонд?(Да/Нет)').lower()
+
+        print('Наступил', d['day'], 'день. Удачи! Цена ресурса 15 руб./шт.')
+        buy = check2()
+        if buy * 15 > d['money']:
+            print('У тебя недостаточно денег, будет куплено: ', d['money'] // 15, 'мороженого')
+            d['money'] -= 15 * (d['money'] // 15)
+            PrintData(d)
+            d['item'] += d['money'] // 15
+            PrintData(d)
+        else:
+            d['money'] -= buy * 15
+            PrintData(d)
+            d['item'] += buy
+            PrintData(d)
+
+        dream = check3()
+
+        sale = randint(1, 100)
+        print('Сегодня клиенты пришли за', sale, 'мороженого.')
+        u = input('Чувствуешь себя успешным? ')
+        print('неважно. хаха')
+
+        if dream >= sale:
+            d['money'] += sale * 25
+            PrintData(d)
+            d['item'] -= sale
+            PrintData(d)
+            d['client'] += randint(5, 10)
+            PrintData(d)
+
+        else:
+            d['client'] -= randint(5, 10)
+            PrintData(d)
+            d['crisis'] += randint(5, 10)
+            PrintData(d)
+            if d['crisis'] == 100:
+                print('Ты проиграл. Увы, бизнес - это не твоё. Кризис достигнул отметки МАКСИМУМ.')
+                break
+
+        answer_donation = input('неважно. хаха! \n Не хотите ли пожертвовать некоторую сумму в благ.фонд?(Да/Нет)').lower()
         while True:
             if answer_donation == 'нет' or answer_donation == 'да':
                 break
@@ -49,8 +113,7 @@ def main():
             if d['money'] < 15:
                 print('Ты прогорел..........')
                 break
-
-        if answer_donation == 'нет':
+        elif answer_donation == 'нет':
             d['client'] -= randint(1, 10)
             PrintData(d)
             print('Эх... Продолжим')
@@ -61,26 +124,26 @@ def main():
                 break
             else:
                 answer_orphan = input('Неверный ввод. Да/Нет?: ').lower()
-
         if answer_orphan == 'нет':
+            PrintData(d)
             print('Жмоотяяяра.')
             d['client'] -= randint(1, 10)
-            PrintData(d)
             d['crisis'] += randint(1, 10)
-            PrintData(d)
             if d['crisis'] >= 100:
                 print('Ты прогорел...(')
                 break
-
-        if answer_orphan == 'да':
+            a = input('Ты еще готов посражаться? ')
+            if a.lower() == 'да':
+                continue
+            else:
+                break
+        elif answer_orphan == 'да':
             help2 = check1()
             d['item'] -= help2
-            PrintData(d)
             d['client'] += randint(5, 15)
             PrintData(d)
             if d['item'] < d['client']:
                 print('Ты прогорел(')
                 break
 
-        d['day'] += 1
 main()
